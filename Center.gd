9d77c847cb0c3033cc3910c20d9a8ae498aae8c5
Ief Vandepitte <ief.vandepitte@telenet.be>
2021-05-11 00:01:54 +0200

    init got rotational movement

---
+++ Center.gd
@@ -0,0 +1,16 @@
+extends Spatial
+export var rotation_speed = PI
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	$MeshInstance/Center.rotate_y(rotation_speed * delta)
---
+++ Level.gd
@@ -0,0 +1,17 @@
+extends Spatial
+
+export var rotation_speed = PI
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	#https://kidscancode.org/godot_recipes/basics/rotation/
+	$Center.rotate_y(rotation_speed * delta)
